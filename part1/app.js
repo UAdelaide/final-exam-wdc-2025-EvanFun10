59622e2431b0de6a5743095198d9dc2f2bb5b6ef
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 19:12:40 +0000

    [automatic commit] 2025-06-20 19:12:40 UTC

--- part1/app.js
+++ part1/app.js
@@ -36,4 +36,6 @@ const setupDatabase = async () => {
         request_id INT AUTO_INCREMENT PRIMARY KEY,
          dog_id INT NOT NULL,
         requested_time DATETIME NOT NULL,
-        
+        duration_minutes INT NOT NULL,
+        location VARCHAR(255) NOT NULL,
+         status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
