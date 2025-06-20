00b22c989a44d03d7853e6eeb23b72a555e8dcd1
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 19:18:09 +0000

    [automatic commit] 2025-06-20 19:18:09 UTC

--- part1/app.js
+++ part1/app.js
@@ -60,4 +60,6 @@ const setupDatabase = async () => {
     await conn.query(`
       CREATE TABLE WalkRatings (
         rating_id INT AUTO_INCREMENT PRIMARY KEY,
+         request_id INT NOT NULL,
+        walker_id INT NOT NULL,
         
