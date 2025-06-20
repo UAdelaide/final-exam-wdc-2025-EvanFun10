3970d7c25439a06992920cdaf1848e59462b8918
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 19:32:58 +0000

    [automatic commit] 2025-06-20 19:32:58 UTC

--- part1/app.js
+++ part1/app.js
@@ -96,4 +96,9 @@ const setupDatabase = async () => {
     `);
 
     // Insert walk requests
+    await conn.query(`
+      INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+      VALUES
+        ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+        
 
