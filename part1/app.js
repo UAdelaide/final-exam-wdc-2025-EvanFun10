7e0fcdcbfe8fab4cf72ac9023354142e980f6d88
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 19:34:05 +0000

    [automatic commit] 2025-06-20 19:34:05 UTC

--- part1/app.js
+++ part1/app.js
@@ -102,6 +102,6 @@ const setupDatabase = async () => {
         ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 10:00:00', 60, 'City Park', 'open'),
-        
+        ((SELECT dog_id FROM Dogs WHERE name = 'Melon'), '2025-06-11 15:00:00', 30, 'Greenwood Lake', 'open'),
 
 
