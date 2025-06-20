920b49115ddcb12acab0435dbb1eab06be806244
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 19:24:04 +0000

    [automatic commit] 2025-06-20 19:24:04 UTC

--- part1/app.js
+++ part1/app.js
@@ -72,3 +72,8 @@ const setupDatabase = async () => {
         CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
       )
     `);
+
+
+    await conn.query(`
+      INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
