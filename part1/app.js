3f0979286d060acf54888a7773135b50ccb4f529
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 19:29:32 +0000

    [automatic commit] 2025-06-20 19:29:32 UTC

--- part1/app.js
+++ part1/app.js
@@ -91,4 +91,5 @@ const setupDatabase = async () => {
         ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
         ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
         ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
+        ('Rocky', 'large', (SELECT user_id FROM Users WHERE username = 'daveowner')),
         
